db68d69207f80438899bb5ceaef36f0cb8f9bb71
Sarvesh Gupta <92543011+SARVESH2120@users.noreply.github.com>
2023-12-28 15:10:30 +0530

    Create MyClass.java

---
+++ Week1/Abstraction/MyClass.java
@@ -0,0 +1,11 @@
+abstract public class MyClass {
+    //complete method
+    public void cal(){
+        System.out.println("Calculating results");
+    }
+    //abstract method
+    abstract public void launchRocket();
+
+}
+
+//abstract class can't be instantiated
