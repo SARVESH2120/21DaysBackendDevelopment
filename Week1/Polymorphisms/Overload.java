7c5fe086fb26ca05eac6cf73e16e926fac8489c8
Sarvesh Gupta <92543011+SARVESH2120@users.noreply.github.com>
2023-12-28 15:46:24 +0530

    Create Overload.java

---
+++ Week1/Polymorphisms/Overload.java
@@ -0,0 +1,8 @@
+public class Overload {
+    public void read(){
+        System.out.println("Student is reading");
+    }
+    public void read(String bookName){
+        System.out.println("Reading"  + bookName);
+    }
+}
