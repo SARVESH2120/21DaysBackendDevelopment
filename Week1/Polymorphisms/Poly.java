9d28e4b9f53c6fefdb2f9b1827a0700b061bc192
Sarvesh Gupta <92543011+SARVESH2120@users.noreply.github.com>
2023-12-28 15:45:53 +0530

    Update Poly.java

---
+++ Week1/Polymorphisms/Poly.java
@@ -0,0 +1,10 @@
+public class Poly {
+    public static void main(String[] args) {
+        Overload ov = new Overload();
+        ov.read();
+        ov.read("Harry Potter");
+    }
+    
+}
+//compile time(Method overloading) ,run time -- polymorphism(method overriding)
+
